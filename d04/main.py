e3305696f582b930d80e79a4d902c28f41ab54ae
Nestor Morales <nestormorales1998@gmail.com>
2023-12-06 20:29:48 +0100

    function

--- d04/main.py
+++ d04/main.py
@@ -4,11 +4,11 @@ with open("input.txt") as f:
     lines = f.readlines()
     input = [line.strip() for line in lines]
 
-score_p1 = 0
-score_cards = defaultdict(lambda: 1)  # to keep track, default 1
-for line in input:
-    card_seeing, score_card_data = line.split(': ')
-    card_seeing = int(card_seeing.split(' ')[-1])
+
+def get_score_card_number_and_matching_numbers(line):
+    """Formatting input."""
+    score_card_number, score_card_data = line.split(': ')
+    score_card_number = int(score_card_number.split(' ')[-1])
 
     score_card_data = score_card_data.strip().replace('  ', ' ')
     winning_numbers, my_numbers = score_card_data.split(' | ')
@@ -16,19 +16,28 @@ for line in input:
     winning_numbers = {int(x.strip()) for x in winning_numbers.split(' ')}  # yapf: disable
     my_numbers = {int(x.strip()) for x in my_numbers.split(' ')}  # yapf: disable
 
-    # did i win?
-    matching = winning_numbers.intersection(my_numbers)
+    matching_numbers = winning_numbers.intersection(my_numbers)  # intersection sets
+
+    return matching_numbers, score_card_number
+
+
+score_p1 = 0
+score_cards = defaultdict(lambda: 1)  # to keep track, default 1
+for line in input:
+    matching_numbers, score_card_number = get_score_card_number_and_matching_numbers(line)
 
     # update score p1
-    if len(matching) > 0:
-        score_p1 += 2**(len(matching) - 1)
+    if len(matching_numbers) > 0:
+        score_p1 += 2**(len(matching_numbers) - 1)
 
-    # update score p2
-    next_score_cards_won = list(range(card_seeing + 1, card_seeing + len(matching) + 1))
-
-    if len(matching) == 0: score_cards[card_seeing] += 0
-    for card in next_score_cards_won:
-        score_cards[card] += score_cards[card_seeing]
+    next_score_cards_won = list(
+        range(score_card_number + 1, score_card_number + len(matching_numbers) + 1)
+    )
+    # update score cards
+    if len(matching_numbers) == 0: score_cards[score_card_number] += 0
+    for won_card in next_score_cards_won:
+        # you win as many score cards as many current cards you have
+        score_cards[won_card] += score_cards[score_card_number]
 
 score_p2 = sum(score_cards.values())
 
